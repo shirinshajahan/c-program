bc16387ec2f50fb90500f583194c46b0652aeac1
shirinshajahan <31505882+shirinshajahan@users.noreply.github.com>
2017-10-08 16:04:53 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the character",ch);
+if(ch=='a'!!ch=='e'!!ch=='i'!!ch=='o'!!ch=='u'!!ch=='A'!!ch=='E'!!ch=='I'!!ch=='O'!!ch=='U')
+{
+printf("character is vowel");
+}
+else
+{
+printf("character id not a vowel");
+}
+}
